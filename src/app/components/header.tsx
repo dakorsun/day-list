60122304313194f0d90bc9829fa9aef49f009230
DK <dakorsun@gmail.com>
2024-10-31 08:39:22 +0200

    some formating and optionality

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -6,9 +6,11 @@ export function HeaderComponent() {
         absolute top-0 left-0 w-screen
         p-[1rem]
         flex flex-row justify-between 
-        text-white font-bold
+        text-white font-bold text-5xl
         ">
         <TimeComponent startingMilliseconds={new Date().getTime()} />
+        {/* <TimeComponent startingMilliseconds={null} /> */}
+        <h1>DL</h1>
     </div>
 
 }
--- src/app/components/time.tsx
+++ src/app/components/time.tsx
@@ -2,13 +2,20 @@
 
 import { useState, useEffect, useMemo } from "react";
 
+function formatSingleDigits(digits: number): string {
+    return digits < 10 ? `0${digits}` : `${digits}`
+}
+
 interface TimeProps {
-    startingMilliseconds: number
+    startingMilliseconds: number | null
 }
 
 export function TimeComponent({ startingMilliseconds }: TimeProps) {
     const [currentDate, setCurrentDate] = useState<Date>(new Date())
     const tracker = useMemo<Date>(() => {
+        if (!startingMilliseconds) {
+            return new Date(0)
+        }
         return new Date(currentDate.getTime() - startingMilliseconds)
     }, [currentDate, startingMilliseconds])
 
@@ -25,13 +32,12 @@ export function TimeComponent({ startingMilliseconds }: TimeProps) {
     }, []);
 
     return <div className="
-        w-1/4
         flex flex-row justify-between 
-        font-bold text-5xl
+        font-bold
         ">
-        <span>{currentDate.getHours()}:{currentDate.getMinutes()}</span>
+        <span>{formatSingleDigits(currentDate.getHours())}:{formatSingleDigits(currentDate.getMinutes())}</span>
         |
-        <span>{tracker.getUTCHours()}:{tracker.getUTCHours()}:{tracker.getUTCSeconds()}</span>
+        <span>{formatSingleDigits(tracker.getUTCHours())}:{formatSingleDigits(tracker.getUTCMinutes())}:{formatSingleDigits(tracker.getUTCSeconds())}</span>
     </div>
 
 }
