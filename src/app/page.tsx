139c90f305960a85358ea6665078104900c76272
DK <dakorsun@gmail.com>
2024-10-31 04:21:00 +0200

    text fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ export default function HomePage() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center text-white">
             <div className="container flex flex-col items-center justify-start gap-12 px-4 py-16">
-                <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+                <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
                     Day List
                 </h1>
-                <span className="text-lg font-bold text-white">Your simple thing to manage your day</span>
+                <span className="text-lg font-bold">Your simple thing to manage your day</span>
             </div>
             <div className="container flex flex-col items-center justify-start gap-12 px-4 py-16">
                 <Link className="text-lg font-bold" href="/test">Test</Link>
