53a8df2dc1d5070bb6594551fd516e13e09fa154
DK <dakorsun@gmail.com>
2025-02-15 09:57:13 +0200

    list layout updates

--- src/components/day-list/day-list.tsx
+++ src/components/day-list/day-list.tsx
@@ -4,7 +4,16 @@ import { DayListItem } from './day-list-item';
 export function DayList() {
 	const items = useDayItemStore(state => state.dayItems);
 	return (
-		<div className="grid grid-cols-3 grid-rows-3 gap-4">
+		<div
+			className="
+			w-full
+			py-[1rem]
+			px-[1rem] lg:px-[2rem] xl:px-[20rem]
+			grid
+			grid-cols-1  xl:grid-cols-2 xl:grid-rows-3
+			grid-rows-3
+			gap-[1rem] xl:gap-[2rem]"
+		>
 			{items.map(item => (
 				<DayListItem item={item} key={item.timestamp} />
 			))}
