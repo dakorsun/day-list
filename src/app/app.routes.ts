106f3a535aceec9231f1adfb529074867a6ee895
DK <dakorsun@gmail.com>
2025-07-05 19:01:13 +0300

    build(app): basic structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Tracker } from '../components/tracker/tracker';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    title: 'root',
+    path: '',
+    component: Tracker,
+  },
+];
---
+++ src/components/tracker/tracker.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tracker',
+  templateUrl: './tracker.html',
+  styleUrl: './tracker.css',
+})
+export class Tracker {}
